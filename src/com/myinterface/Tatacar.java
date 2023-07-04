daaa5e9ef8499d14fa0653eeeb4321587cb08f03
Dipali Pujari <121038391+dipalipujari02@users.noreply.github.com>
2023-07-04 21:32:28 +0530

    Update Tatacar.java

--- src/com/myinterface/Tatacar.java
+++ src/com/myinterface/Tatacar.java
@@ -1,6 +1,6 @@
 package com.myinterface;
 
-public class Tatacar implements Car{
+public class Tatacar implements Car{       //implements keyword is used to impliment interface
     @Override
     public void drive() {
         System.out.println("Tata car is 4 wheel drive");
